a57123c4a25174252802600ee9bbe93c82c9cb82
McNieps <lucas.kosinski@ext.apicil.fr>
2023-07-18 18:27:55 +0200

    [sprit] Multiple change. Sprites are now lighter

--- ise/environment/sprite/debug_sprite.py
+++ ise/environment/sprite/debug_sprite.py
@@ -1,12 +1,21 @@
-import ise.environment.location as ise_loc
+import pygame
+
+from ise.environment.sprite.simple_sprite import SimpleSprite
 
 
-class DebugSprite:
+class DebugSprite(SimpleSprite):
     def __init__(self,
-                 location: type[ise_loc.StaticLoc]) -> None:
+                 radius: int = 9) -> None:
 
-        pass
+        surface = pygame.Surface((radius*2+1, radius*2+1))
+        surface.set_colorkey((0, 0, 0))
+
+        pygame.draw.line(surface, (255, 0, 0), (radius+1, radius+1), (radius*2+1, radius+1), 1)
+        pygame.draw.line(surface, (0, 255, 0), (radius+1, radius+1), (radius+1, 0), 1)
+        pygame.draw.ellipse(surface, (0, 0, 255), pygame.Rect(radius, radius, 3, 3))
+
+        super().__init__(surface)
 
 
 if __name__ == '__main__':
-    x = DebugSprite(ise_loc.AdvancedLocation)
+    x = DebugSprite()
--- ise/environment/sprite/simple_sprite.py
+++ ise/environment/sprite/simple_sprite.py
@@ -1,2 +1,33 @@
+import pygame
+
+from collections.abc import Iterable
+
+
 class SimpleSprite:
-    pass
+    __slots__ = ["surface", "rect", "effective_rect"]
+
+    def __init__(self,
+                 surface: pygame.Surface) -> None:
+
+        self.surface = surface
+        self.rect = self.surface.get_rect()
+        self.rect.center = 0, 0
+
+        self.effective_rect = self.rect.copy()
+
+    def update(self,
+               delta: float) -> None:
+        pass
+
+    def render(self,
+               destination: pygame.Surface,
+               destination_rect: pygame.Rect,
+               offset: Iterable,
+               _angle: float) -> None:
+
+        self.effective_rect = self.rect.move(*offset)
+
+        if not self.effective_rect.colliderect(destination_rect):
+            return
+
+        destination.blit(self.surface, self.effective_rect)
