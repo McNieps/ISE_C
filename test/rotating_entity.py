cf1cc5416b667fbc6cf5f4c610324682b3009d05
McNieps <mcnieps@gmail.com>
2023-07-23 13:50:10 +0200

    [tests] Modified rotating entity

--- test/rotating_entity.py
+++ test/rotating_entity.py
@@ -1,5 +1,3 @@
-import pygame
-
 from ise.environment.entity import Entity
 from ise.environment.sprite import DebugSprite
 from ise.environment.position import AdvancedPos
@@ -10,10 +8,8 @@ class RotatingEntity(Entity):
         position = AdvancedPos(position=(200, 200), speed=(1, 0), va=10)
 
         super().__init__(position, DebugSprite())
+        self.sprite.set_rendering_technique("rotated")
 
     def update(self,
                delta: float) -> None:
         super().update(delta)
-        # self.position.position.rotate_ip(-self.position.va*delta)
-        self.position.speed = pygame.Vector2(self.position.speed.magnitude(), 0).rotate(-self.position.a)
-        print(self.position.a)
