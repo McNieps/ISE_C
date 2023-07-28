eb8ae9a6c26ec6ea94e430b97c14c8f9927f3ea7
McNieps <mcnieps@gmail.com>
2023-07-28 17:27:24 +0200

    [packg] Added import in package for easier use

---
+++ ise/app/__init__.py
@@ -0,0 +1,4 @@
+from ise.app.app import App
+from ise.app.resource import Resource
+
+__all__ = [App, Resource]
---
+++ ise/environment/__init__.py
@@ -0,0 +1,6 @@
+from ise.environment.entity import Entity
+from ise.environment.scene import Scene
+from ise.environment.camera import Camera
+from ise.environment import position
+
+__all__ = [Entity, Scene, Camera, position]
